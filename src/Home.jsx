d3da666e2d741de0d4f077570bd6296d5adc95e7
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2023-01-12 09:05:28 +0530

    added clear button

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 		// event.preventDefault();
 		let char = text.split("").length;
 		let wc = "";
-		char == 0 ? wc = 0 : wc = text.split(" ").length;
+		char == 0 ? wc = 0 : wc = text.trim().split(" ").length;
 		
 		let msg = "Word count: " + wc + ",  " + "Character count: " + char;
 		setAns(msg);
@@ -18,15 +18,21 @@ const Home = () => {
 
 	useEffect(() => {find()}, [text])
 
+	const clear = (event) => {
+		event.preventDefault();
+		setText("");
+	}
+
 	return (
 		<>
 			<div className="min-h-[87.5vh]  flex flex-col items-center">
 				<h1 className="text-white  font-bold text-3xl my-5">Word Counter</h1>
-				<form className="flex flex-col items-center font-mono" onSubmit={find}>
-					<textarea className="hidden lg:block bg-stone-800 text-white p-4 my-4 rounded-lg resize-none focus:outline-none focus:ring focus:ring-orange-300" rows={10} cols={70} onChange={hText}></textarea>
-					<textarea className="hidden sm:block lg:hidden bg-stone-800 text-white p-4 my-4 rounded-lg resize-none focus:outline-none focus:ring focus:ring-orange-300" rows={10} cols={50} onChange={hText}></textarea>
+				<form className="flex flex-col items-center font-mono" onSubmit={clear}>
+					<textarea value={text} className="hidden lg:block bg-stone-800 text-white p-4 my-4 rounded-lg resize-none focus:outline-none focus:ring focus:ring-orange-300" rows={10} cols={70} onChange={hText}></textarea>
+					<textarea value={text} className="hidden sm:block lg:hidden bg-stone-800 text-white p-4 my-4 rounded-lg resize-none focus:outline-none focus:ring focus:ring-orange-300" rows={10} cols={50} onChange={hText}></textarea>
 					<textarea className="sm:hidden bg-stone-800 text-white p-4 my-4 rounded-lg resize-none focus:outline-none focus:ring focus:ring-orange-300" rows={10} cols={30} onChange={hText}></textarea>
-					<input type="submit" className="bg-white w-[20%] rounded-md my-2"></input>
+					<input type="submit" value="Clear"
+						className="bg-white w-[20%] rounded-md my-2"></input>
 				</form>
 				<h2 className="text-white mt-3">{ans}</h2>
 			</div>
